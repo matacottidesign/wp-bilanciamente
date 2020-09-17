67df5f7fc00a4f294765e665257f42ee61e3ea23
Francesco Matacotti <francesco1984.go@gmail.com>
2020-09-17 20:20:45 +0200

    impostato navbar di base

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -18,6 +18,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 		$theme_version = $the_theme->get( 'Version' );
 
 		$css_version = $theme_version . '.' . filemtime( get_template_directory() . '/css/theme.min.css' );
+		wp_enqueue_style( 'google-font', 'https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;700&family=Yanone+Kaffeesatz:wght@300;400;700&display=swap');
 		wp_enqueue_style( 'understrap-styles', get_template_directory_uri() . '/css/theme.min.css', array(), $css_version );
 
 		wp_enqueue_script( 'jquery' );
