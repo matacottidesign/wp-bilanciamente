fa211ae286d8ad28ef17abca982782ad468e7f14
Francesco Matacotti <francesco1984.go@gmail.com>
2020-09-18 16:13:26 +0200

    aggiustato link TORNA SU

--- js/theme.js
+++ js/theme.js
@@ -7009,12 +7009,13 @@
     }, false);
   }
 })();
-console.log("hello");
-console.log(document.getElementsByClassName('understrap-read-more-link'));
+console.log("hello"); //SOSTITUIRE "Leggi di più..." con "LEGGI ARTICOLO"
+
 let nodeList_btnReadMore = document.getElementsByClassName('understrap-read-more-link');
 let a;
 
 for (a = 0; a < nodeList_btnReadMore.length; a++) {
   console.log(nodeList_btnReadMore[a].innerHTML = "LEGGI ARTICOLO");
   nodeList_btnReadMore[a].classList.remove("btn-secondary");
+  nodeList_btnReadMore[a].parentNode.classList.add("bottone-leggi-articolo");
 }
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,10 +1,10 @@
 console.log("hello");
-console.log(document.getElementsByClassName('understrap-read-more-link'));
 
+//SOSTITUIRE "Leggi di più..." con "LEGGI ARTICOLO"
 let nodeList_btnReadMore = document.getElementsByClassName('understrap-read-more-link');
-
 let a;
 for(a=0; a < nodeList_btnReadMore.length; a++){
     console.log(nodeList_btnReadMore[a].innerHTML = "LEGGI ARTICOLO");
     nodeList_btnReadMore[a].classList.remove("btn-secondary");
+    nodeList_btnReadMore[a].parentNode.classList.add("bottone-leggi-articolo");
 }
