f83092753e0ece586fdc818959427c83743c3122
Francesco Matacotti <francesco1984.go@gmail.com>
2020-09-19 20:48:56 +0200

    completato tutte le pagine del sito

--- page-templates/esercizi.php
+++ page-templates/esercizi.php
@@ -72,7 +72,7 @@ while ( have_posts() ) :
                                 $link = get_sub_field('link_card_esercizi');
                                 if( $link ): 
                                     $link_url = $link['url'];
-                                    $link_title = $link['title'];
+                                    //$link_title = $link['title'];
                                     $link_target = $link['target'] ? $link['target'] : '_self';
                                     ?>
                                     <a class="btn understrap-read-more-link" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
--- page-templates/libri.php
+++ page-templates/libri.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Blog
+ * Template Name: Libri
  *
  * Template for displaying a page just with the header and footer area and a "naked" content area in between.
  * Good for landingpages and other types of pages where you want to add a lot of custom markup.
@@ -21,11 +21,11 @@ while ( have_posts() ) :
     <div class="container">
         <div class="row banner d-flex align-items-center">
             <div class="col-12 col-md-6">
-                <h1><?php the_field('titolo_pagina_blog') ?></h1>
+                <h1><?php the_field('titolo_pagina_libri') ?></h1>
             </div>
             <div class="col-12 col-md-6 text-right">
                 <?php 
-                $image = get_field('immagine_pagina_blog');
+                $image = get_field('immagine_pagina_libri');
                 if( !empty( $image ) ): ?>
                     <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
                 <?php endif; ?>
@@ -41,35 +41,26 @@ while ( have_posts() ) :
             
         <div class="row">
             
-            <?php if( have_rows('ripetitore_card_blog') ):
-            while( have_rows('ripetitore_card_blog') ) : the_row(); ?>
+            <?php if( have_rows('ripetitore_card_libri') ):
+            while( have_rows('ripetitore_card_libri') ) : the_row(); ?>
                 <div class="col-12 col-md-6 mt-3">
-                    <div style="background-color: #e9dfdd;" class="type-post">
+                    <div style="background-color: #d1d6e6;" class="type-post">
                         <div class="entry-content">
                             <!--IMG-->
                             <?php 
-                            $image = get_sub_field('immagine_card_blog');
+                            $image = get_sub_field('immagine_card_libri');
                             if( !empty( $image ) ): ?>
                                 <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
                             <?php endif; ?>
                             <!--TITOLO-->
-                            <h2 class="card-title"><?php the_sub_field('titolo_card_blog') ?></h2><hr>
-                            <h3 style="color: #adb5bd;">Categoria: <?php 
-                            $link = get_sub_field('link_categoria');
-                            if( $link ): 
-                                $link_url = $link['url'];
-                                $link_title = $link['title'];
-                                $link_target = $link['target'] ? $link['target'] : '_self';
-                                ?>
-                                <a href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-                            <?php endif; ?></h3>
-                            <h4 style="color: #adb5bd;">Tempo lettura: <?php the_sub_field('tempo_lettura');?></h4>
+                            <h2 class="card-title"><?php the_sub_field('titolo_card_libri') ?></h2><hr>
+                            <h4 style="color: #adb5bd;"><?php the_sub_field('autore');?></h4>
                             <!--RIASSUNTO-->
-                            <?php the_sub_field('riassunto_card_blog') ?>
+                            <?php the_sub_field('riassunto_card_libri') ?>
                             <!--BOTTONE-->
                             <div class="bottone-leggi-articolo">
                                 <?php 
-                                $link = get_sub_field('link_card_blog');
+                                $link = get_sub_field('link_recensione');
                                 if( $link ): 
                                     $link_url = $link['url'];
                                     //$link_title = $link['title'];
