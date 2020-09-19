0b843e077a59306c07fa97ae1a54a0857e50eb34
Francesco Matacotti <francesco1984.go@gmail.com>
2020-09-19 19:38:08 +0200

    completato ESERCIZI e corretto BLOG

--- page-templates/esercizi.php
+++ page-templates/esercizi.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Blog
+ * Template Name: Esercizi
  *
  * Template for displaying a page just with the header and footer area and a "naked" content area in between.
  * Good for landingpages and other types of pages where you want to add a lot of custom markup.
@@ -21,11 +21,11 @@ while ( have_posts() ) :
     <div class="container">
         <div class="row banner d-flex align-items-center">
             <div class="col-12 col-md-6">
-                <h1><?php the_field('titolo_pagina_blog') ?></h1>
+                <h1><?php the_field('titolo_pagina_esercizi') ?></h1>
             </div>
             <div class="col-12 col-md-6 text-right">
                 <?php 
-                $image = get_field('immagine_pagina_blog');
+                $image = get_field('immagine_pagina_esercizi');
                 if( !empty( $image ) ): ?>
                     <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
                 <?php endif; ?>
@@ -41,19 +41,19 @@ while ( have_posts() ) :
             
         <div class="row">
             
-            <?php if( have_rows('ripetitore_card_blog') ):
-            while( have_rows('ripetitore_card_blog') ) : the_row(); ?>
+            <?php if( have_rows('ripetitore_card_esercizi') ):
+            while( have_rows('ripetitore_card_esercizi') ) : the_row(); ?>
                 <div class="col-12 col-md-6 mt-3">
-                    <div style="background-color: #e9dfdd;" class="type-post">
+                    <div style="background-color: #ecdc94;" class="type-post">
                         <div class="entry-content">
                             <!--IMG-->
                             <?php 
-                            $image = get_sub_field('immagine_card_blog');
+                            $image = get_sub_field('immagine_card_esercizi');
                             if( !empty( $image ) ): ?>
                                 <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
                             <?php endif; ?>
                             <!--TITOLO-->
-                            <h2 class="card-title"><?php the_sub_field('titolo_card_blog') ?></h2><hr>
+                            <h2 class="card-title"><?php the_sub_field('titolo_card_esercizi') ?></h2><hr>
                             <h3 style="color: #adb5bd;">Categoria: <?php 
                             $link = get_sub_field('link_categoria');
                             if( $link ): 
@@ -65,11 +65,11 @@ while ( have_posts() ) :
                             <?php endif; ?></h3>
                             <h4 style="color: #adb5bd;">Tempo lettura: <?php the_sub_field('tempo_lettura');?></h4>
                             <!--RIASSUNTO-->
-                            <?php the_sub_field('riassunto_card_blog') ?>
+                            <?php the_sub_field('riassunto_card_esercizi') ?>
                             <!--BOTTONE-->
                             <div class="bottone-leggi-articolo">
                                 <?php 
-                                $link = get_sub_field('link_card_blog');
+                                $link = get_sub_field('link_card_esercizi');
                                 if( $link ): 
                                     $link_url = $link['url'];
                                     $link_title = $link['title'];
