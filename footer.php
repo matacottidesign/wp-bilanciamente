6fd3b9cede5729cd579e2f9023b11b8e4dd9e8f7
Francesco Matacotti <francesco1984.go@gmail.com>
2020-09-19 15:28:42 +0200

    corretto illustrazioni + credit

--- footer.php
+++ footer.php
@@ -21,6 +21,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<div class="col-6 col-md-3 footer-col pb-5">
 				<div class="logo-svg mb-5"></div>
 				<span>Orgogliosamente sviluppato con il <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-suit-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"/></svg> da <a style="color: #a1daf8;" href="matacottidesign.it">Matacotti Design</a></span>
+				<a href="http://www.freepik.com"> Progettato da slidesgo / Freepik </a>
 			</div>
 
 			<div class="col-6 col-md-3 footer-col pb-5">
