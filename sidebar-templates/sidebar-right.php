f6f82263a547ef6225385174f2f24fb2c4e47872
Francesco Matacotti <francesco1984.go@gmail.com>
2020-09-20 18:56:08 +0200

    importato iframe facebook

--- sidebar-templates/sidebar-right.php
+++ sidebar-templates/sidebar-right.php
@@ -21,6 +21,7 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 <?php else : ?>
 	<div class="col-md-4 widget-area" id="right-sidebar" role="complementary">
 		<div id="right-widget-col" class="sticky-top">
+		<!-- <iframe style="margin-top: 1rem;" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbilanciamente&tabs&width=340&height=130&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=false&appId" width="340" height="130" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe> -->
 <?php endif; ?>
 <?php dynamic_sidebar( 'right-sidebar' ); ?>
 
