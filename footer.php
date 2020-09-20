e5a98c4f48464ec9849c8a2f8e80199b570af060
Francesco Matacotti <francesco1984.go@gmail.com>
2020-09-20 19:14:15 +0200

    aggiunto bottone torna su

--- footer.php
+++ footer.php
@@ -81,6 +81,14 @@ $container = get_theme_mod( 'understrap_container_type' );
 	</div>
 </footer>
 
+<button type="button" class="btn-tornasu btn fixed-bottom mb-3 ml-3">
+<a href="#">
+<svg width="35px" height="35px" viewBox="0 0 16 16" class="bi bi-arrow-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+</svg>
+</a>
+</button>
+
 </div><!-- #page we need this extra closing tag here -->
 
 <?php wp_footer(); ?>
